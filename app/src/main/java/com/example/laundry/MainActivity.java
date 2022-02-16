f3df66379ad833cd76ca1e2d29667c47f763f3ab
jyj10 <jyj10201000@gmail.com>
2022-02-17 02:00:31 +0900

    커밋 푸쉬 되는 지 확인

--- app/src/main/java/com/example/laundry/MainActivity.java
+++ app/src/main/java/com/example/laundry/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    //commit and push 되는지 확인
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
